68441d145914a3dcdffb60f4e6acd3a89f12fff4
Tavi03 <taviurdu@yahoo.com>
2024-01-23 22:17:14 +0200

    My solve on the HandsOn exercise

--- main.cpp
+++ main.cpp
@@ -1,32 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
+string specializare;
+
+struct Doctor {
+    string nume, specializare;
+    string caz;
+};
+
+struct Pacient {
+    string problema, specializare;
+};
+
+bool eLiber(Doctor doctor) {
+    if ((doctor.caz == "") && (doctor.specializare == specializare)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main() {
     ifstream inFile("HandsOn-Input.txt");
 
-    int no_problems, no_doctors;
+    vector<Pacient> pacienti;
+    vector<Doctor> doctori;
     string name, speciality;
-    
-    inFile >> no_problems;
 
-    for (int i = 0; i < no_problems; i++)
-    {
+    int no_problems, no_doctors;
+
+    inFile >> no_problems;
+    for (int i = 0; i < no_problems; i++) {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        pacienti.push_back({ name, speciality });
     }
 
     inFile >> no_doctors;
-
-    for (int i = 0; i < no_doctors; i++)
-    {
+    for (int i = 0; i < no_doctors; i++) {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctori.push_back({ name, speciality });
+    }
+
+    for (vector<Pacient>::iterator j = pacienti.begin(); j < pacienti.end(); j++) {
+        specializare = j->specializare;
+        vector<Doctor>::iterator it = find_if(doctori.begin(), doctori.end(), eLiber);
+        if (it != doctori.end()) {
+            it->caz = j->problema;
+            cout << it->nume << " " << it->caz << "\n";
+        }
     }
 
     return 0;
